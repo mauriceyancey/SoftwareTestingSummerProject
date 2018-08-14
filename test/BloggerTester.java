c35475b92c4e998baf81e8dbb29d46d75c4f6db3
Memo Leon <gleonoliva@users.noreply.github.com>
2018-08-13 20:58:52 -0500

    Added OAuth2 and a test
    This one wasn't easy I tried several ways to get the token in a more automated fashion.
    I ended up using the 3rd option, copy&paste for [mobile & desktop apps](https://developers.google.com/identity/protocols/OAuth2InstalledApp). Perhaps can be automated even further using [restAssured API](https://github.com/rest-assured/rest-assured/wiki/usage#csrf) I could not figure out how :(
    
    For now works pretty good. One can run main method in OAuth2Client in order to get the access_token and refresh_token. The workflow requires you to open an URI login and copy and paste a code back to the console. Once that's done you can now run your tests that need access_token, you can see an example in BloggerTester.java

--- test/BloggerTester.java
+++ test/BloggerTester.java
@@ -1,4 +1,7 @@
+import AuthClient.OAuth2Client;
+import AuthClient.OAuth2ClientCredentials;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Test;
 
@@ -17,6 +20,8 @@ public class BloggerTester {
     String baseURI = "https://www.googleapis.com/blogger/v3/blogs/";
     String apiKey = "AIzaSyAYHQw2hPRA5yAdz7A3I7qUo43qa-aZUCM";
     String blogID = "54850391780151973";
+
+    final String SCOPE = "https://www.googleapis.com/auth/blogger";
 
 
     //GET https://www.googleapis.com/blogger/v3/blogs/54850391780151973?key=AIzaSyAYHQw2hPRA5yAdz7A3I7qUo43qa-aZUCM
@@ -56,5 +61,23 @@ public class BloggerTester {
                         .statusCode(200)
                         .extract().response();
         System.out.println(response.asString());
+    }
+
+    @Test
+    public void getUserBlogs()
+    {
+        OAuth2Client.authorize();
+
+        RestAssured.baseURI = "https://www.googleapis.com/blogger/v3/users/self/blogs";
+        Response response =
+                given()
+                    .auth().oauth2(OAuth2ClientCredentials.AccessToken)
+                    .contentType(ContentType.JSON)
+                .expect()
+                    .log().all()
+                    .statusCode(200)
+                    .when()
+                .get().then().extract().response();
+        System.out.println(response.toString());
     }
 }
