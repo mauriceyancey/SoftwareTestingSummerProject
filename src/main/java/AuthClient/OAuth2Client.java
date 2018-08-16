b914eb00afd97c692c643a73e8105cd30a594466
Memo Leon <gleonoliva@users.noreply.github.com>
2018-08-15 22:47:10 -0500

    Changed the way credentials are stored to a safer use of DataStoreFactory

--- src/main/java/AuthClient/OAuth2Client.java
+++ src/main/java/AuthClient/OAuth2Client.java
@@ -1,45 +1,68 @@
 package AuthClient;
 
+import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.DataStoreFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class OAuth2Client {
 
-    private static final String CREDENTIAL_FILE_PATH = "credentials.token";
+    private static final String DATA_STORE_DIR = "credentials.store";
     private static final String SCOPE = "https://www.googleapis.com/auth/blogger";
+
+    private static DataStoreFactory storeFactory;
+
+    private static String accessToken = null;
+
+    private static String CREDENTIAL_ID = "soft-test-user";
 
     private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
     static final JsonFactory JSON_FACTORY = new JacksonFactory();
 
     private static GoogleAuthorizationCodeFlow initializeFlow() throws IOException {
         return new GoogleAuthorizationCodeFlow.Builder(
-                new NetHttpTransport(), JacksonFactory.getDefaultInstance(),
+                HTTP_TRANSPORT, JSON_FACTORY,
                 OAuth2ClientCredentials.CLIENT_ID, OAuth2ClientCredentials.CLIENT_SECRET,
-                Collections.singleton(SCOPE)).build();
+                Collections.singleton(SCOPE)).setDataStoreFactory(getStoreFactory()).build();
+    }
+
+    private static DataStoreFactory getStoreFactory() {
+        if (storeFactory == null) {
+            try {
+                File credFile = new File(DATA_STORE_DIR);
+                storeFactory = new FileDataStoreFactory(credFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return storeFactory;
     }
 
     // Based on: https://gitlab.tubit.tu-berlin.de/klems/googledrivejavaexample/blob/5688e4bc907424036a5b9857f33d3bd50b51b29e/src/com/google/drive/example/DriveCommandLine.java
     public static void authorize() {
-        if (validCredentialsStored()) {
-            System.out.println("Valid credentials stored. No need to re-authorize.");
-            System.out.printf("You can also delete the file %s to force a re-auth.\n", CREDENTIAL_FILE_PATH);
-            return;
-        }
-
         GoogleAuthorizationCodeFlow flow = null;
         try {
             flow = initializeFlow();
+            Credential c = flow.loadCredential(CREDENTIAL_ID);
+
+            if (c != null) {
+                accessToken = c.getAccessToken();
+                System.out.println("Valid credentials stored. No need to re-authorize.");
+                System.out.printf("You can also delete the file %s to force a re-auth.\n", DATA_STORE_DIR);
+                return;
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -55,77 +78,20 @@ public class OAuth2Client {
         GoogleTokenResponse response = null;
         try {
             response = flow.newTokenRequest(code).setRedirectUri(OAuth2ClientCredentials.COPY_PASTE_REDIRECT_URI).execute();
+            System.out.println("Authenticated correctly! :D");
+            System.out.println("Saving credentials to disk...");
+            Credential c = flow.createAndStoreCredential(response, CREDENTIAL_ID);
+            accessToken = c.getAccessToken();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        GoogleCredential credential = (new GoogleCredential.Builder())
-                .setJsonFactory(new JacksonFactory())
-                .setTransport(HTTP_TRANSPORT)
-                .setClientSecrets(OAuth2ClientCredentials.CLIENT_ID, OAuth2ClientCredentials.CLIENT_SECRET)
-                .build().setFromTokenResponse(response);
-
-        System.out.println("Authenticated correctly! :D");
-        System.out.println(response);
-        System.out.println(credential);
-
-        OAuth2ClientCredentials.AccessToken = response.getAccessToken();
-        OAuth2ClientCredentials.RefreshToken = response.getRefreshToken();
-        OAuth2ClientCredentials.TokenObtainedMillis = System.currentTimeMillis();
-        OAuth2ClientCredentials.TokenExpiresInMillis = System.currentTimeMillis() + response.getExpiresInSeconds() * 1000;
-
-        System.out.println("Saving credentials to disk...");
-        saveCredentials();
     }
 
-    private static boolean validCredentialsStored() {
-        File f = new File(CREDENTIAL_FILE_PATH);
-        if (f.exists()) {
-            try {
-                FileInputStream fis = new FileInputStream(f);
-                ObjectInputStream ois = new ObjectInputStream(fis);
-
-                Map<String, String> keyMap = (Map<String, String>) ois.readObject();
-                OAuth2ClientCredentials.AccessToken = keyMap.get("accessToken");
-                OAuth2ClientCredentials.RefreshToken = keyMap.get("refreshToken");
-                OAuth2ClientCredentials.TokenExpiresInMillis = Long.parseLong(keyMap.get("expiresInMillis"));
-                OAuth2ClientCredentials.TokenObtainedMillis = Long.parseLong(keyMap.get("obtainsInMillis"));
-
-                if (System.currentTimeMillis() > OAuth2ClientCredentials.TokenExpiresInMillis) {
-                    System.out.println("Access Token Expired...");
-                    return false;
-                }
-                return true;
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-
+    public static String getAccessToken() {
+        if (accessToken == null) {
+            authorize();
         }
-        return false;
-    }
-
-    private static void saveCredentials() {
-        try {
-            FileOutputStream fos = new FileOutputStream(CREDENTIAL_FILE_PATH);
-            Map<String, String> keyMap = new HashMap<String, String>();
-            keyMap.put("accessToken", OAuth2ClientCredentials.AccessToken);
-            keyMap.put("refreshToken", OAuth2ClientCredentials.RefreshToken);
-            keyMap.put("expiresInMillis", Long.toString(OAuth2ClientCredentials.TokenExpiresInMillis));
-            keyMap.put("obtainsInMillis", Long.toString(OAuth2ClientCredentials.TokenObtainedMillis));
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-
-            oos.writeObject(keyMap);
-
-            oos.close();
-            fos.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        return accessToken;
     }
 
     public static void main(String[] args) {
